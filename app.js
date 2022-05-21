aacc19ada68769e9e7915277c64720df59a983ed
diego1005 <dmaguilar82@gmail.com>
2022-05-21 10:10:27 -0300

    make first step  main section home

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+//settings
 const port = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: false}));
 
 //routes
 app.get("/", (req, res) => {
