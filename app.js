76d888a4ffb010a2b003009e10a09f9c26dc8c8e
Astor Levy <102769883+AstorLevy@users.noreply.github.com>
2022-05-22 17:23:39 -0300

    add: login v1

--- app.js
+++ app.js
@@ -2,19 +2,29 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-//settings
+//settings___________________________________________________
 const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false}));
 
-//routes
+//routes____________________________________________________
+
+//Home
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"));
 })
+//Log in
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
+})
+//Sing in
+app.get("/singin", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/singin.html"));
+})
 
-//static
+//static____________________________________________________
 app.use(express.static(path.join(__dirname, "public")));
 
-//server
+//server____________________________________________________
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
 })
