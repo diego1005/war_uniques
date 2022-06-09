721ef71411288fb891325dab7f230f05813ad2e7
Astor Levy <102769883+AstorLevy@users.noreply.github.com>
2022-06-08 22:16:25 -0300

    add: sell.ejs/css, sell in products.routers and productsController

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -5,5 +5,6 @@ const productsController = require("../controller/productsController");
 
 router.get("/detail", productsController.detail);
 router.get("/cart", productsController.cart);
+router.get("/sell", productsController.sell);
 
 module.exports = router;
