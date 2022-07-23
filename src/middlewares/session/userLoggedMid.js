4dfad90164081b212b297e423e449e9e82986b49
diego1005 <dmaguilar82@gmail.com>
2022-07-23 10:19:04 -0300

    add userUnlogged redirect

--- src/middlewares/session/userLoggedMid.js
+++ src/middlewares/session/userLoggedMid.js
@@ -1,6 +1,8 @@
 const userLoggedMid = (req, res, next) => {
     const userLogged = req.session.userLogged;
-    (userLogged) ? res.render("perfil", { user : userLogged }) : next(); 
+    (userLogged) ? res.render("perfil", { user: userLogged }) : res.redirect("/");
+    next();
 }
+
 
 module.exports = userLoggedMid;
