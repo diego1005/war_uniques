2a6ac4a3265ddd88db099db2a5f1e0b56caa1353
diego1005 <dmaguilar82@gmail.com>
2022-08-09 16:21:38 -0300

    better style dropdown menu

--- public/js/viewJS/main.js
+++ public/js/viewJS/main.js
@@ -1,5 +1,5 @@
 let displayMenu = () => {
     let tag = document.querySelector(".nav-user-content");
     console.log(tag);
-    tag.style.display = (tag.style.display == "none") ? "block" : "none";
+    tag.style.display = (tag.style.display == "none") ? "flex" : "none";
 }
