829c3627f18b3aaff129600a4366e8f3f4b9bf7d
bflavia272 <bflavia272@gmail.com>
2022-08-27 21:17:32 -0300

    Validación

--- public/js/loginValidator.js
+++ public/js/loginValidator.js
@@ -1,1 +1,24 @@
-console.log("lo hice bien")
+var Usuario = document.getElementById('Usuario');
+var password = document.getElementById('password');
+var error = document.getElementById('error');
+error.style.color ='black';
+
+
+function enviarFormulario(){
+   console.log("enviando el formulario");
+
+   var mensajesError=[];
+   if(Usuario.value === null || Usuario.value === '') {
+      mensajesError.push('Ingresa tu Usuario');
+   }
+   
+   if(password.value === null || password.value === ''){
+      mensajesError.push('Ingresa tu Contraseña');
+   }
+   error.innerHTML=mensajesError.join(',');
+   return false;
+    
+}
+
+
+
