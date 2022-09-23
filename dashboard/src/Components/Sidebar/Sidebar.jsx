e4ddc71a71bade2ac3b2df69455ab9d1dae072bd
Astor Levy <102769883+AstorLevy@users.noreply.github.com>
2022-09-22 22:55:02 -0300

    update: logo

--- dashboard/src/Components/Sidebar/Sidebar.jsx
+++ dashboard/src/Components/Sidebar/Sidebar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import "./Sidebar.css"
+import Logo from '../../Assets/img/WAR_UNIQUES-minimal.jpg'
 
 function Sidebar() {
   return (
-    <div className='sidebar'>Hola soy el sidebar</div>
+    <div>
+      <div className='sidebar'>Hola soy el sidebar</div>
+      <img className='logo' src={Logo} alt="Logo"/>
+    </div>
   )
 }
 
