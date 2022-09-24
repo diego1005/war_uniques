e0b0abda1114dac02ef31c1288c2a930c7533e6b
diego1005 <dmaguilar82@gmail.com>
2022-09-24 12:37:18 -0300

    add more styles dashboard

--- dashboard/src/Components/Footer/Footer.jsx
+++ dashboard/src/Components/Footer/Footer.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
 import "./Footer.css"
-import InstagramImg from "../../Assets/img/instagram.png"
-import WhatsappImg from "../../Assets/img/whatsapp.png"
-import EmailImg from "../../Assets/img/email.png"
 
 function Footer() {
   return (
     <div className='footer'>
+      <div className="footleft">
+        © 2022, made with <i className="fa-solid fa-heart"></i> by <a className="anchor-foot" href="#"><b>Grupo 9</b></a> for a better web.
+      </div>
+      <div className='footright'>
+        <ul className='footlist'>
+          <li>
+            <a href="#">Grupo 9</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+          <li>
+            <a href="#">License</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
+
+/*
+
+import InstagramImg from "../../Assets/img/instagram.png"
+import WhatsappImg from "../../Assets/img/whatsapp.png"
+import EmailImg from "../../Assets/img/email.png"
+
       <div className='fotleft'>
             <p>Politica y privacidad</p>
             <p>Terminos y condiciones</p>
@@ -18,14 +46,10 @@ function Footer() {
             <p>Av. Caseros 3039, Piso 2, CP 1264, Parque Patricios, CABA</p>
         </div>
         <div className="fotright">
-            <div class="contactos">
+            <div className="contactos">
                 <div><img src={InstagramImg} alt="iglogo" /></div>
                 <div><img src={WhatsappImg} alt="wsplogo" /></div>
                 <div><img src={EmailImg} alt="emaillogo" /></div>
             </div>
         </div>
-    </div>
-  )
-}
-
-export default Footer
+*/
--- dashboard/src/Components/Main/MainSection/MainSection.jsx
+++ dashboard/src/Components/Main/MainSection/MainSection.jsx
@@ -5,12 +5,15 @@ import imagen from "../../../Assets/img/imagen_pueba.webp";
 function MainSection() {
   return (
     <div className='main-section'>
-      <div className="main-section-image">
-        <img src={imagen} alt="imagen de prueba" className="main-image" />
-      </div>
-      <div className="main-section-description">
-        <h1 className='main-title'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, voluptatibus?</h1>
-        <p className='main-desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error sint, consequuntur ex et est quaerat cupiditate reiciendis dolorum laboriosam, facere sunt labore officiis modi. Vel sunt dolor facere commodi impedit, expedita eveniet nostrum facilis neque ipsa laudantium atque id. Cupiditate aliquam labore eveniet, esse veniam atque est! Ipsa exercitationem veniam maiores atque provident impedit quasi eveniet quibusdam, nemo vero incidunt voluptates placeat tempora enim quo, quis, molestiae rem veritatis ad saepe labore. Obcaecati magni unde incidunt minus, blanditiis aperiam quo rerum illo porro delectus debitis excepturi nihil hic repellat temporibus explicabo aspernatur deserunt quidem nesciunt quod nobis sunt ut dolores! Voluptatibus molestias adipisci repellat tempore mollitia repudiandae eligendi, officiis expedita ipsam, nisi placeat quam laudantium illo consectetur. Illum, odio explicabo? Cupiditate exercitationem neque animi autem impedit sapiente ea adipisci.</p>
+      <h2 className='main-section-title'>Last Product</h2>
+      <div className="contain">
+        <div className="main-section-image">
+          <img src={imagen} alt="imagen de prueba" className="main-image" />
+        </div>
+        <div className="main-section-description">
+          <h1 className='main-title'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, voluptatibus?</h1>
+          <p className='main-desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error sint, consequuntur ex et est quaerat cupiditate reiciendis dolorum laboriosam, facere sunt labore officiis modi. Vel sunt dolor facere commodi impedit, expedita eveniet nostrum facilis neque ipsa laudantium atque id. Cupiditate aliquam labore eveniet, esse veniam atque est! Ipsa exercitationem veniam maiores atque provident impedit quasi eveniet quibusdam, nemo vero incidunt voluptates placeat tempora enim quo, quis, molestiae rem veritatis ad saepe labore. Obcaecati magni unde incidunt minus, blanditiis aperiam quo rerum illo porro delectus debitis excepturi nihil hic repellat temporibus explicabo aspernatur deserunt quidem nesciunt quod nobis sunt ut dolores! Voluptatibus molestias adipisci repellat tempore mollitia repudiandae eligendi, officiis expedita ipsam, nisi placeat quam laudantium illo consectetur. Illum, odio explicabo? Cupiditate exercitationem neque animi autem impedit sapiente ea adipisci.</p>
+        </div>
       </div>
     </div>
   )
