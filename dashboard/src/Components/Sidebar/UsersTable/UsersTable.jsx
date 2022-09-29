c36c63d7b9e50594e925881fbb0a6bf06447e66d
Astor Levy <102769883+AstorLevy@users.noreply.github.com>
2022-09-28 23:17:04 -0300

    add: user imgs

--- dashboard/src/Components/Sidebar/UsersTable/UsersTable.jsx
+++ dashboard/src/Components/Sidebar/UsersTable/UsersTable.jsx
@@ -4,6 +4,8 @@ import "./UsersTable.css";
 function UsersTable(props) {
   const [users, setUsers] = useState([]);
 
+  const urlApiImgUser = "http://localhost:3001/images/user-image/";
+  
   useEffect(() => {
     console.log("%cSe monto el componente", "color: green");
     fetch("http://localhost:3001/api/users")
@@ -21,7 +23,7 @@ function UsersTable(props) {
             <div><h4>NOMBRE: </h4><p>{user.name} {user.lastname}</p></div>
             <div><h4>ID: </h4><p>{user.id}</p></div>
             <div><h4>EMAIL:</h4><p>{user.email}</p></div>
-            <div className="avatar"><img src={user.imageURL} alt="avatar"/></div>
+            <div className="avatar"><img src={urlApiImgUser + user.imageURL} alt="avatar"/></div>
           </div>
         );
       })}
